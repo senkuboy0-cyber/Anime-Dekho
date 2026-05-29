894b899a81ce7c7299bc27d7b454cc80965ec5ac
senkuboy0-cyber <senkuboy0@gmail.com>
2026-05-29 11:55:33 +0600

    Uncomment OnePace provider registration

--- AnimeDekhoProvider/src/main/kotlin/com/anime/AnimeDekhoPlugin.kt
+++ AnimeDekhoProvider/src/main/kotlin/com/anime/AnimeDekhoPlugin.kt
@@ -12,7 +12,7 @@ import com.lagradost.cloudstream3.extractors.Voe
 class AnimeDekhoPlugin: BasePlugin() {
     override fun load() {
         registerMainAPI(AnimeDekhoProvider())
-        //registerMainAPI(OnepaceProvider())
+        registerMainAPI(OnepaceProvider())
         registerMainAPI(HindiSubAnime())
         registerExtractorAPI(StreamRuby())
         registerExtractorAPI(Vidmolynet())
