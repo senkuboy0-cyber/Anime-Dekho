5fc4479b3ef32ab6c7f47a178c3c326d8021d516
senkuboy0-cyber <255331092+senkuboy0-cyber@users.noreply.github.com>
2026-09-23 23:30:14 +0600

    Create build.gradle.kts

---
+++ MyAnimesProvider/build.gradle.kts
@@ -0,0 +1,17 @@
+version = 1
+
+cloudstream {
+    language = "hi"
+    authors = listOf("senkuboy0-cyber")
+    description = "MyAnimes: Hindi, Tamil, Telugu, English multi-audio anime, series and movies."
+    status = 1
+    tvTypes = listOf(
+        "Anime",
+        "AnimeMovie",
+        "Cartoon",
+        "TvSeries",
+        "Movie"
+    )
+    iconUrl = "https://myanimes.in/wp-content/uploads/2026/09/cropped-ChatGPT-Image-Sep-21-2026-08_48_22-AM.png"
+    isCrossPlatform = true
+}
